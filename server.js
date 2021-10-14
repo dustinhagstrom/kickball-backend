02edbe611b0a27111b870ba2a30720ebcd151dbb
dustnhgstrm <77700279+dustnhgstrm@users.noreply.github.com>
2021-10-14 08:20:40 -0500

    changed the server port variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const app = require("./app");
-const port = 8080;
+const port = 3001;
 
 mongoose
   .connect(process.env.MONGO_DB, {
